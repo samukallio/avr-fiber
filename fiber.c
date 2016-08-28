a1c04eeb515ba28d3c35c45c92eec1fd04839d7f
Samu Kallio <samu@samukallio.net>
2016-08-28 17:56:35 +0300

    Fix wake() replacing currently running fiber
    So, obviously it doesn't work to have the current FCB be
    the head of the run queue, since a higher-priority fiber
    may replace it in the queue while it's still executing.
    Make a separate run queue and modify yield() and wake()
    as necessary to fix that embarrassing oversight.
    
    While at it, namespace the privately used symbols to recude
    the chances of a collision, and tidy up the public interface
    function argument naming, too.

--- fiber.c
+++ fiber.c
@@ -5,39 +5,38 @@
 
 typedef uint16_t pgm_intptr_t;
 
-struct fiber _main_fiber = { 0 };
-struct fiber *volatile _current = &_main_fiber;
+struct fiber _f_main = { 0 };
+struct fiber *_f_current = &_f_main;
+struct fiber *_f_queue_head = 0;
 
-void _fiber_entry();
+void _f_entry();
 
-int spawn(struct fiber *f, uint8_t priority, uint8_t *stack, size_t stack_size,
-           void (*fn)(void *), void *arg)
+int spawn(struct fiber *f, uint8_t priority, uint8_t *stack_base,
+          size_t stack_size, void (*entry)(void *), void *argument)
 {
-	uint8_t *sp;
+	uint8_t *stack;
 
 	if (stack_size < FIBER_STACK_SIZE_MIN)
 		return -1;
 
-	sp = stack + stack_size - 1;
+	stack = stack_base + stack_size - 1;
 
 	/* Put the entry point into the stack, so that the first 'ret'
-	   actually takes us at the start of _fiber_entry. */
-	*sp-- = (pgm_intptr_t) _fiber_entry & 0xFF;
-	*sp-- = (pgm_intptr_t) _fiber_entry >> 8;
+	   actually takes us at the start of _f_entry. */
+	*stack-- = (pgm_intptr_t) _f_entry & 0xFF;
+	*stack-- = (pgm_intptr_t) _f_entry >> 8;
 
 	/* Argument-passing registers are caller-save and hence not stored in
 	   the context, so we use callee-save registers to pass arguments to
 	   a special assembly entry routine which then loads them into their
 	   correct argument registers. */
-	*sp-- = (intptr_t) arg & 0xFF;
-	*sp-- = (intptr_t) arg >> 8;
-	*sp-- = (pgm_intptr_t) fn & 0xFF;
-	*sp-- = (pgm_intptr_t) fn >> 8;
+	*stack-- = (intptr_t) argument & 0xFF;
+	*stack-- = (intptr_t) argument >> 8;
+	*stack-- = (pgm_intptr_t) entry & 0xFF;
+	*stack-- = (pgm_intptr_t) entry >> 8;
 
 	/* Leave r6-r17, r28, r29 unspecified. */
-	sp -= 14;
-
-	f->sp = sp;
+	f->stack = stack - 14;
 	f->priority = priority;
 
 	wake(f);
@@ -47,5 +46,5 @@ int spawn(struct fiber *f, uint8_t priority, uint8_t *stack, size_t stack_size,
 
 struct fiber *current()
 {
-	return _current;
+	return _f_current;
 }
--- fiber.h
+++ fiber.h
@@ -8,13 +8,13 @@
 /* Fiber Control Block (FCB) */
 struct fiber {
 	struct fiber *next;
-	uint8_t *sp;
+	uint8_t *stack;
 	uint8_t priority;
 };
 
 /* Start a new fiber with the given priority, executing the given function. */
-int spawn(struct fiber *f, uint8_t priority, uint8_t *stack, size_t stack_size,
-          void (*fn)(void *), void *arg);
+int spawn(struct fiber *f, uint8_t priority, uint8_t *stack_base,
+	  size_t stack_size, void (*entry)(void *), void *argument);
 
 /* Return the FCB of the currently executing fiber. */
 struct fiber *current();
