bb5046a1120a3a588eac4679163bacd8d2aa7e39
Samu Kallio <samu@samukallio.net>
2016-08-28 03:17:52 +0300

    Add some comments on the fiber.h API

--- fiber.h
+++ fiber.h
@@ -3,6 +3,7 @@
 
 #define FIBER_STACK_SIZE	128
 
+/* Fiber Control Block (FCB) */
 struct fiber {
 	struct fiber *next;
 	uint8_t *stackp;
@@ -10,9 +11,16 @@ struct fiber {
 	unsigned char stack[FIBER_STACK_SIZE];
 };
 
+/* Start a new fiber with the given priority, executing the given function. */
 void spawn(struct fiber *f, int priority, void (*fn)(void *), void *arg);
+
+/* Return the FCB of the currently executing fiber. */
 struct fiber *current();
+
+/* Schedule the given fiber for execution according to its priority. */
 void wake(struct fiber *f);
+
+/* Suspend the current fiber until awoken. */
 void yield();
 
 #endif
